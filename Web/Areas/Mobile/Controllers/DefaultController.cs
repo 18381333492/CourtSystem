5ab9c04db5813d29fd5835d03a268fd4e0e7e69e
Administrator <Administrator@MS-20170511IAEE>
2017-10-13 16:24:38 +0800

    123456789

---
+++ Web/Areas/Mobile/Controllers/DefaultController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Areas.Mobile.Controllers
+{
+    public class DefaultController : Controller
+    {
+        //
+        // GET: /Mobile/Default/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
